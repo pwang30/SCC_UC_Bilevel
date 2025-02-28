28a7ebfbfacf1a694816b801e98a01887a9a9012
peng wang <powerwang2017@gmail.com>
2025-02-28 16:20:35 +0100

    Update admittance_matrix_calculation.jl

--- single_level_UC_SCL/admittance_matrix_calculation.jl
+++ single_level_UC_SCL/admittance_matrix_calculation.jl
@@ -7,7 +7,7 @@ function admittance_matrix_calculation(numnodes)
     for k in 1:branch_num               #  calculate the ADMITTANCE MATRIX of the transmission lines
         i = linepara[k, 1]                  # bus from
         j = linepara[k, 2]                   # bus to
-        Yₗᵢₙₑ[i, j] = -1/linepara[k, 4]*4        # off-diagonal elements
+        Yₗᵢₙₑ[i, j] = -1/linepara[k, 4]*2        # off-diagonal elements
         Yₗᵢₙₑ[j, i] = Yₗᵢₙₑ[i, j]                # symmetry
     end
     for k in 1:numnodes
